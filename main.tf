2107d6d5806466002eecc314bbed8dd660d95c71
Nabi <nabisaheb7780@gmail.com>
2023-12-11 19:40:32 +0530

    Expense poject setup

--- main.tf
+++ main.tf
@@ -3,5 +3,10 @@ module "vpc" {
   vpc_cidr_block = var.vpc_cidr_block
   tags = var.tags
   env = var.env
+  app_subnets = var.app_subnets
+  db_subnets = var.db_subnets
+  public_subnets = var.public_subnets
+  web_subnets = var.web_subnets
+  azs = var.azs
 }
 
