d2567153583d45487b5bd1f325ea28c1c15c54ed
Nabi <nabisaheb7780@gmail.com>
2023-12-29 10:06:42 +0530

    Terraform

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "main" {
 
 
 resource "aws_launch_template" "main" {
-  name_prefix   = "${var.env}-${var.component}"
+  name          = "${var.env}-${var.component}"
   image_id      = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
@@ -40,6 +40,9 @@ resource "aws_launch_template" "main" {
     role_name = var.component
     env   = var.env
   }))
+  iam_instance_profile {
+    name = aws_iam_instance_profile.main.name
+  }
 }
 
 resource "aws_autoscaling_group" "main" {
@@ -60,4 +63,53 @@ resource "aws_autoscaling_group" "main" {
   }
 }
 
+resource "aws_iam_role" "main" {
+  name = "${var.env}-${var.component}"
+  tags = merge(var.tags, { Name = "${var.env}-${var.component}" })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
+  inline_policy {
+    name = "SSM-read-access"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "GetResources ",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource": "arn:aws:ssm:us-east-1:146677740583:parameter/${var.env}.${var.component}.*"
+        },
+        {
+          "Sid": "ListResources",
+          "Effect": "Allow",
+          "Action": "ssm:DescribeParameters",
+          "Resource": "*"
+        }
+      ]
+    })
+  }
+
+}
+
+resource "aws_iam_instance_profile" "main"{
+  name = "${var.env}-${var.component}"
+  role = aws_iam_role.main.name
+}
